56e9c9d57a5650263be609e285e8a99fcd8dd0f5
unknown <wilfriedngassarou23@gmail.com>
2025-01-02 22:18:10 +0100

    Feat: end of interaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,98 @@
+import { motion } from "motion/react"
+import { useState } from "react"
+import { LocationSVG } from "./components/location-svg";
+import { AddressItem } from "./components/address-item";
+import { Credits } from "./components/credits";
 
 function App() {
-  return (
-    <section className="w-full h-dvh bg-gray-200">
+  const addresses = [
+    {
+      label: 'Deido Roundabout',
+      title: 'street',
+      img: 'street.webp'
+    },
+    {
+      label: 'Douala',
+      title: 'city',
+      img: 'city.webp'
+    },
+    {
+      label: 'Littoral',
+      title: 'region',
+      img: 'region.webp'
+    },
+    {
+      label: '2450',
+      title: 'zip',
+      img: 'zip.webp'
+    },
+    {
+      label: 'Cameroon',
+      title: 'country',
+      img: 'country.webp'
+    },
+  ]
+  const fullAddress = addresses.reduce((a, b) => ( a + `${b.label}, `), '').slice(0, -2) ;
+  
+  const [flexDirection, setFlexDirection] = useState<'row' | 'column'>('row')
 
+  return (
+    <section className="w-full h-dvh flex items-center justify-center bg-gray-50">
+      <Credits />
+      <div className="fixed z-[99] bg-white flex items-center justify-center inset-0 md:hidden">
+        <span className="text-lg">
+          Please use a medium screen.
+        </span>
+      </div>
+
+      <motion.section
+        initial={{ x: -190, opacity: 0 }}
+        animate={{ 
+          x: flexDirection == 'row' ? -190 : -160,
+          opacity: flexDirection == 'row' ? 0 : 1
+        }} 
+        transition={{ type: "tween", duration: .3 }}
+        className="absolute"
+      >
+        {addresses.map((item, index) => (
+          <span
+            key={index}
+            className="flex items-center h-9 uppercase"
+          >
+            <span className="text-sm">
+              {item.title}
+            </span>
+          </span>
+        ))}
+      </motion.section>
+      <section 
+        className="flex"
+        style={{ flexDirection }}
+      >
+        {addresses.map((item, index, array) => (
+          <AddressItem
+            key={index}
+            flexDirection={flexDirection}
+            isLastIndex={(array.length - 1) == index}
+            item={item} 
+          />
+        ))}
+      </section>
+      <footer className="fixed left-0 bottom-0 w-full h-14 bg-gray-100 flex items-center justify-center gap-4">
+        <span
+          style={{ boxShadow: '0px 0px 2px rgb(0 0 0 / 0.4)' }} 
+          className="bg-white text-gray-600 rounded-lg p-2 text-sm"
+        >
+          {fullAddress}
+        </span>
+        <span 
+          style={{ boxShadow: '0px 0px 2px rgb(0 0 0 / 0.4)' }} 
+          onClick={() => setFlexDirection(flexDirection == 'row' ? 'column' : 'row')}
+          className="bg-white text-gray-600 rounded-lg p-2 cursor-pointer"
+        >
+          <LocationSVG />
+        </span>
+      </footer>
     </section>
   )
 }
---
+++ src/components/address-item.tsx
@@ -0,0 +1,51 @@
+import clsx from "clsx"
+import { motion } from "motion/react"
+
+interface Props {
+  item: {
+    label: string,
+    title: string,
+    img: string,
+  },
+  isLastIndex: boolean,
+  flexDirection: 'row' | 'column'
+}
+
+export function AddressItem({ item, isLastIndex, flexDirection }: Props) {
+  return (
+    <motion.span
+      layout="position"
+      style={{ fontSize: 24 }}
+      className="cursor-pointer address relative group font-medium"
+    >
+      <span className="text-black black">
+        {item.label}
+        {flexDirection == 'row' && !isLastIndex && ','}
+      </span>
+      <span className="text-blue-700 blue">
+        {item.label}
+        {flexDirection == 'row' && !isLastIndex && ','}
+      </span>
+      <div 
+        className={clsx(
+          "absolute min-w-40 w-full -top-44 left-0 flex justify-center scale-50 opacity-0 translate-y-20 duration-300",
+          flexDirection == 'row' && "group-hover:scale-100 group-hover:opacity-100 group-hover:translate-y-0 group-hover:z-50"
+        )}
+      >
+        <img 
+          alt={item.title} 
+          src={item.img} 
+          className="w-40 h-40 border-4 border-white -rotate-3 rounded-xl overflow-hidden"
+        />
+      </div>
+      <span 
+        className={clsx(
+          "absolute left-0 w-full text-gray-400 text-center text-sm top-10 whitespace-nowrap scale-50 opacity-0 duration-300 uppercase -translate-y-4",
+          flexDirection == 'row' && "group-hover:scale-100 group-hover:opacity-100 group-hover:translate-y-0"
+        )}
+      >
+        {item.title}
+      </span>
+    </motion.span>
+  )
+}
